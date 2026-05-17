b8b9c69ccfa7d1c08ca8e4353fae1474239351f7
Abrar Faiz Mohammed <131560669+AbrarFaizMohammed@users.noreply.github.com>
2026-05-17 11:27:40 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 100
+float_type = 99.99
+bool_type = False
+string_type = 'Hello Abrar!'
+list_type = [1,2,3,4]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
