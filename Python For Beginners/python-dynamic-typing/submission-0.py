8bfd68d4b29c4677e4289a6f6520faecb683b432
Abrar Faiz Mohammed <131560669+AbrarFaizMohammed@users.noreply.github.com>
2026-05-17 11:30:00 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 100
+print(type(variable))
+variable = 99.99
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Abrar"
+print(type(variable))
+variable = [1,2,3,4]
+print(type(variable))
